5a6de9110f46e9d69592124b4f38f6e2e98135fe
pyabzz <>
2020-07-25 19:56:54 +0100

    Add initialSetup

---
+++ Win10/Scripts/InitialSetup.ps1
@@ -0,0 +1,12 @@
+Add-Type -AssemblyName 'PresentationFramework'
+
+$ShouldProceed = 'No'
+
+$ShouldProceed = [System.Windows.MessageBox]::Show("Disable automatic monitor turning off?", "Please confirm", 'YesNo');
+
+If ($ShouldProceed -eq 'Yes') {
+    powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
+}
+Else {
+    Write-Output 'Skipped'  
+} 
