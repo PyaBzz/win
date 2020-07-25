b65d47cdea612b03f1e8af5f6a6e59d18060d8d3
PyaBzz <>
2020-07-25 23:58:09 +0100

    Reference reg files in the initial script

--- Win10/Scripts/InitialSetup.ps1
+++ Win10/Scripts/InitialSetup.ps1
@@ -1,12 +1,42 @@
 Add-Type -AssemblyName 'PresentationFramework'
 
+#==================  Disable automatic monitor turning off  ==================
 $ShouldProceed = 'No'
-
 $ShouldProceed = [System.Windows.MessageBox]::Show("Disable automatic monitor turning off?", "Please confirm", 'YesNo');
 
-If ($ShouldProceed -eq 'Yes') {
+if ($ShouldProceed -eq 'Yes') {
     powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 }
-Else {
+else {
+    Write-Output 'Skipped'  
+} 
+
+#==================  Remove Background Images  ==================
+$ShouldProceed = [System.Windows.MessageBox]::Show("Remove lock screen and background of the login screen and wallpaper?", "Please confirm", 'YesNo');
+
+if ($ShouldProceed -eq 'Yes') {
+    reg import "..\RegistryTweaks\Backgrounds.reg"
+}
+else {
+    Write-Output 'Skipped'  
+} 
+
+#==================  Tidy up Taskbar  ==================
+$ShouldProceed = [System.Windows.MessageBox]::Show("Tidy up the taskbar?", "Please confirm", 'YesNo');
+
+if ($ShouldProceed -eq 'Yes') {
+    reg import "..\RegistryTweaks\Taskbar.reg"
+}
+else {
+    Write-Output 'Skipped'  
+} 
+
+#==================  Restart  ==================
+$ShouldProceed = [System.Windows.MessageBox]::Show("Restart for the changes to take effect?", "Please confirm", 'YesNo');
+
+if ($ShouldProceed -eq 'Yes') {
+    shutdown /r /t 0
+}
+else {
     Write-Output 'Skipped'  
 } 
