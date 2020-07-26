e8e4c06da16855aad5eb3b2da5c736b01ab43a76
PyaBzz <>
2020-07-26 18:32:17 +0100

    Refactor PopupConfirm function

--- Win10/Scripts/Functions.ps1
+++ Win10/Scripts/Functions.ps1
@@ -1,5 +1,14 @@
-function bazUnpin([string]$appname){
-    $ErrorActionPreference= 'silentlycontinue'
-    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object {$_.Name -eq $appname}).Verbs() | Where-Object {$_.Name.replace('&','') -match 'Unpin from taskbar'} | ForEach-Object {$_.DoIt();}
-    $ErrorActionPreference= 'continue'
+Add-Type -AssemblyName 'PresentationFramework'
+
+function PopupConfirm([string]$question) {
+    $Local:ShouldProceed = 'No'
+    $ShouldProceed = [System.Windows.MessageBox]::Show($question, "Please confirm", 'YesNo');
+    
+    if ($ShouldProceed -eq 'Yes') { $true } else { $false } 
+}
+
+function bazUnpin([string]$appname) {
+    $ErrorActionPreference = 'silentlycontinue'
+    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $appname }).Verbs() | Where-Object { $_.Name.replace('&', '') -match 'Unpin from taskbar' } | ForEach-Object { $_.DoIt(); }
+    $ErrorActionPreference = 'continue'
 }
--- Win10/Scripts/InitialSetup.ps1
+++ Win10/Scripts/InitialSetup.ps1
@@ -1,47 +1,40 @@
-Add-Type -AssemblyName 'PresentationFramework'
 .".\Functions.ps1"
 
-#==================  Disable automatic monitor turning off  ==================
+#======================================================
 $ShouldProceed = 'No'
-$ShouldProceed = [System.Windows.MessageBox]::Show("Disable automatic monitor turning off?", "Please confirm", 'YesNo');
+$ShouldProceed = PopupConfirm("Disable automatic monitor turning off?");
 
-if ($ShouldProceed -eq 'Yes') {
+if ($ShouldProceed) {
     powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 }
 else {
     Write-Output 'Skipped'  
 } 
 
-#==================  Remove Background Images  ==================
-$ShouldProceed = [System.Windows.MessageBox]::Show("Remove lock screen and background of the login screen and wallpaper?", "Please confirm", 'YesNo');
+#======================================================
+$ShouldProceed = PopupConfirm("Remove background images?");
 
-if ($ShouldProceed -eq 'Yes') {
+if ($ShouldProceed) {
     reg import "..\RegistryTweaks\Backgrounds.reg"
 }
 else {
     Write-Output 'Skipped'  
 } 
 
-#==================  Tidy up Taskbar  ==================
-$ShouldProceed = [System.Windows.MessageBox]::Show("Tidy up the taskbar?", "Please confirm", 'YesNo');
+#======================================================
+$ShouldProceed = PopupConfirm("Clean-up the taskbar?");
 
-if ($ShouldProceed -eq 'Yes') {
+if ($ShouldProceed) {
     reg import "..\RegistryTweaks\Taskbar.reg"
-    bazUnpin "Microsoft Edge"
-    bazUnpin "Microsoft Store"
-    bazUnpin "Store"
-    bazUnpin "File Explorer"
-    bazUnpin "Explorer"
-    bazUnpin "Mail"
 }
 else {
     Write-Output 'Skipped'  
 } 
 
-#==================  Restart  ==================
-$ShouldProceed = [System.Windows.MessageBox]::Show("Restart for the changes to take effect?", "Please confirm", 'YesNo');
+#======================================================
+$ShouldProceed = PopupConfirm("Restart for the changes to take effect?");
 
-if ($ShouldProceed -eq 'Yes') {
+if ($ShouldProceed) {
     shutdown /r /t 0
 }
 else {
